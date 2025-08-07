d0d30cde75392f7f24ec563afb6cf717c8da5c51
ruwanthiThe <ruthennakoon@gmail.com>
2025-08-08 00:51:49 +0530

    commit

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Header = () => {
+  return (
+    <div className='flex flex-col md:flex-row flex-wrap bg-indigo-500 rounded-lg px-6 md:px-10 lg:px-20'>
+      
+      {/*------left side-----*/}
+      <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px] '>
+        <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>
+            Book Appointment with <br /> Doctor
+        </p>
+        <div className='flex flx-col md:flex-row items-center gap-3 text-white text-sm font-light '>
+            <img className='w-28' src ={assets.group_profiles} alt=""/>
+            <p>Find the best doctor for you <br className='hidden sm:block'/>
+            Schedule your appointment online and get the care you need.</p>
+        </div>
+        <a href="#speciality" className='flex items-center gap-2 bg-gradient-to-r from-teal-400 to-teal-600 px-6 py-3 rounded-full text-white font-semibold shadow-lg hover:from-teal-500 hover:to-teal-700 transition-colors duration-200 hover:scale-105'>
+            Book Appointment
+            <img src={assets.arrow_icon} alt="" className='w-5 h-5 '/>
+        </a>
+      </div>
+
+
+
+      {/*------right side-----*/}
+      <div className='md:w-1/2 flex items-center justify-center'>
+        <img className='w-5000 md:w-5/6 h-auto rounded-lg' src={assets.header_img} alt=""/>
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,11 +10,19 @@ const Navbar = () => {
     navigate('/login');
   }
 
+  const handleProfileClick = () => {
+    navigate('/my-profile');
+  }
+
+  const handleAppoinmentsClick = () => {
+    navigate('/my-appoinments');
+  }
+
   const [showMenu, setShowMenu] = useState(false)
   const[token, setToken] = useState(true)
 
   return (
-    <div className='flex justify-between items-center bg-white p-4 shadow-md'>
+    <div className='flex justify-between items-center bg-white p-4 shadow-md border-b-2 border-gray-300 mb-3'>
       <img className='w-44 cursor -pointer' src={assets.logo} alt="" />
       <ul className='hidden md:flex items-start gap-5 font-medium '>
         <NavLink to='/'>
@@ -40,6 +48,14 @@ const Navbar = () => {
           <div className='flex items-center gap-2 cursor-pointer group relative'>
             <img className='w-8 rounded-full' src={assets.profile_pic} alt=""/>
             <img className='w-2.5' src={assets.dropdown_icon} alt=""/>
+
+            <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+              <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                <p className='hover:text-black cursor-pointer' onClick={handleProfileClick}>My Profile</p>
+                <p className='hover:text-black cursor-pointer' onClick={handleAppoinmentsClick}>My Appoinments</p>
+                <p className='hover:text-black cursor-pointer' onClick={() => setToken(false)}>Logout</p>
+              </div>
+            </div>
           </div>
           :<button  className='bg-indigo-500 text-white px-8 py-3 rounded-full hidden md:block hover:bg-indigo-600 transition-colors duration-200 cursor-pointer' onClick={handleCreateAccount}>Create Account</button>
         }
